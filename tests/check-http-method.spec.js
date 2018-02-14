aa7807c119fdb38e8b4f95fa317f812b67cf75ab
Alexander Ermakov <Alexander.Ermakov@sidenis.com>
2018-02-14 18:02:09 +0300

    Apply recomendations.

--- tests/check-http-method.spec.js
+++ tests/check-http-method.spec.js
@@ -3,18 +3,16 @@ const checkHttpMethod = require('../lib/check-http-method');
 
 describe('checkHttpMethod function ->', () => {
     HTTP_METHODS.forEach(method => {
-        it(`should checked as true for "${method}" method`, done => {
+        it(`should treat HTTP method "${method}" as a correct method`, done => {
             const result = checkHttpMethod(method);
             expect(result).toBe(true);
             done();
         });
     });
 
-    [null, undefined, NaN, 'wrong_method', 42].forEach(method => {
-        it(`should checked as false for incorrect "${method}" method`, done => {
-            const result = checkHttpMethod(method);
-            expect(result).toBe(false);
-            done();
-        });
+    it(`should be able to detect an incorrect http method`, done => {
+        const result = checkHttpMethod('incorrect_method');
+        expect(result).toBe(false);
+        done();
     });
 });
